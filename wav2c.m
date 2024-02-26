38875829b465bcd4ab773737071ce6c149d4a8c4
Florian Paul Schmidt <mista.tapas@gmx.net>
2024-02-26 13:56:46 +0100

    fix all warnings

--- wav2c.m
+++ wav2c.m
@@ -1,22 +1,24 @@
 # printf ("%s", program_name ());
 arg_list = argv ();
-printf("struct IR { float sample_rate; unsigned n_samples; float sample_data[]; };\n\n")
+printf("struct IR { float sample_rate; unsigned n_samples; float *sample_data; };\n\n")
 
 
 for i = 1:nargin
-  printf("struct IR IR%d = {\n", i)
+  printf("static float sample_data%d[] = {\n", i)
   [sample_data, sample_rate] = audioread(arg_list{i});
-  printf("  %f,\n", sample_rate);
-  printf("  %d,\n", size(sample_data, 1));
-  printf("  {\n");
   for j = 1:size(sample_data, 1)
-    printf("    %.18f,\n", sample_data(j, 1));
-  endfor
-  printf("  }\n");
+    printf("  %.24f,\n", sample_data(j, 1));
+  end
   printf("};\n\n");
-endfor
-printf("struct IR *IRs[] = {\n")
+end
+printf("static struct IR IRs[] = {\n")
 for i = 1:nargin
-  printf("  &IR%d,\n", i);
-endfor
-printf("};\n")
+  printf("  {\n");
+  printf("    %f,\n", sample_rate);
+  printf("    %d,\n", size(sample_data, 1));
+  printf("    sample_data%d,\n", i)
+  printf("  },\n")
+end
+printf("};\n\n");
+printf("static unsigned n_IR = %d;\n\n", nargin);
+
