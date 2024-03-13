cafe767dd710a870e8818e2453b39021523c67b5
Florian Paul Schmidt <mista.tapas@gmx.net>
2024-03-13 13:48:15 +0100

    fix name

--- create.m
+++ create.m
@@ -9,12 +9,16 @@ number_of_irs = nargin - 1;
 printf("Name: %s\n", arg_list{1})
 printf("Number of IRs: %d\n", number_of_irs)
 
-system(sprintf("cat manifest_template.ttl | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > manifest.ttl", arg_list{1}, number_of_irs));
+plugin_dir = sprintf("lv2/fixed-ir-%s.lv2", arg_list{1})
 
-system(sprintf("cat plugin_template.ttl | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > plugin.ttl", arg_list{1}, number_of_irs));
+system(sprintf("mkdir -p %s", plugin_dir))
 
-system(sprintf("cat plugin_template.cc | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > plugin.cc", arg_list{1}, number_of_irs));
+system(sprintf("cat manifest_template.ttl | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > %s/manifest.ttl", arg_list{1}, number_of_irs, plugin_dir))
 
-system(sprintf("cat makefile_template | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > makefile", arg_list{1}, number_of_irs));
+system(sprintf("cat plugin_template.ttl | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > %s/plugin.ttl", arg_list{1}, number_of_irs, plugin_dir))
+
+system(sprintf("cat plugin_template.cc | sed -e 's/COLLECTION/%s/g' | sed -e 's/NUMBER_OF_IRS/%d/g' > plugin.cc", arg_list{1}, number_of_irs))
+
+system(sprintf("cat makefile_template | sed -e 's/COLLECTION/%s/g' | sed -e 's;PLUGIN_DIR;%s;g' > makefile", arg_list{1}, plugin_dir))
 
 
