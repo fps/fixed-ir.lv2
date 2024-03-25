fbf86583d1bf80e8c650b99a465a34b17388fb7f
Florian Paul Schmidt <mista.tapas@gmx.net>
2024-03-25 12:32:33 +0100

    doh

--- fixed-ir-wavs2plugin.sh
+++ fixed-ir-wavs2plugin.sh
@@ -21,7 +21,7 @@ number_of_irs=${#IRS[@]}
 for (( index=0; index<${number_of_irs};index++ )); do
   IR="${IRS[$index]}"
   echo IR: "$index": "$IR"
-  ./wav2c "$IR" "$index" >> "$out"/irs.c
+  fixed-ir-wav2c "$IR" "$index" >> "$out"/irs.c
 done
 
 echo "struct IR *IRs[] = {" >> $out/irs.c
